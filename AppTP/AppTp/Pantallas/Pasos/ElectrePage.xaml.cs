3705b304bc432cb6dde32bf84245b261c4953afd
Rusgo <107321881+Rusgo@users.noreply.github.com>
2024-06-06 22:11:00 -0300

    grafico electre

--- AppTP/AppTp/Pantallas/Pasos/ElectrePage.xaml.cs
+++ AppTP/AppTp/Pantallas/Pasos/ElectrePage.xaml.cs
@@ -1,3 +1,4 @@
+using AppTp.Entidades;
 using AppTp.Metodos;
 using System.Runtime.CompilerServices;
 
@@ -6,7 +7,10 @@ namespace AppTp.Pantallas.Pasos;
 public partial class ElectrePage : TabbedPage
 {
     Electre electre;
-	public ElectrePage(Electre electre)
+    int count = 0;
+    private List<Nodo> nodos = new List<Nodo>();
+    private List<Arista> aristas = new List<Arista>();
+    public ElectrePage(Electre electre)
 	{
 		InitializeComponent();
         this.electre = electre;
@@ -31,6 +35,70 @@ public partial class ElectrePage : TabbedPage
         electre.di = float.Parse(d1.Text);
         electre.agregacion(electre.matrizNormalizada.GetLength(0), electre.matrizNormalizada.GetLength(1));
         Metodos.formatoTabla.CreateTable(Entidades.formatoAhp.formatoExcel(electre.matrizSuperacion, true, ""), "Supera", GridSupe);
+        float ultimonodoparx = 0;
+        float ultimonodopary = 0;
+        float ultimonodoimparx = 0;
+        float ultimonodoimpary = 0;
+
+        float[,] matriz = electre.matrizSuperacion;
+
+        // Crear una instancia de Random para generar números aleatorios
+        Random random = new Random();
+
+        
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+
+            if (i == 0)
+            {
+                Nodo nodo = new Nodo("Nodo " + (i + 1), 30, 30);
+                nodos.Add(nodo);
+                ultimonodoparx = nodo.X;
+                ultimonodopary = nodo.Y;
+
+            }
+
+            if (i % 2 == 0 && i > 1)
+            {
+                Nodo nodo = new Nodo("Nodo " + (i + 1), ultimonodoparx + 100, ultimonodopary);
+                nodos.Add(nodo);
+                ultimonodoparx = nodo.X;
+                ultimonodopary = nodo.Y;
+            }
+            if (i % 2 != 0 && i > 1)
+            {
+                Nodo nodo = new Nodo("Nodo " + (i + 1), ultimonodoimparx + 100, ultimonodoimpary);
+                nodos.Add(nodo);
+                ultimonodoimparx = nodo.X;
+                ultimonodoimpary = nodo.Y;
+            }
+            if (i == 1)
+            {
+                Nodo nodo = new Nodo("Nodo " + (i + 1), ultimonodoparx, ultimonodopary + 200);
+                nodos.Add(nodo);
+                ultimonodoimparx = nodo.X;
+                ultimonodoimpary = nodo.Y;
+            }
+
+
+
+        }
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (matriz[i, j] == 1)
+                {
+                    Arista arista = new Arista(nodos[i], nodos[j]);
+                    aristas.Add(arista);
+                }
+            }
+        }
+        var drawable = new GrafoDrawable(nodos, aristas);
+        graphView.Drawable = drawable;
+        graphView.Invalidate();
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
